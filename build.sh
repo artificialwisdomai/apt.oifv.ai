e629f5c1bf44b55b15ae573942c712974bdf4590
Steven Dake <sdake@computelify.ai>
2022-10-27 07:25:48 -0700

    Update to place in debian distribtion

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-DEBIAN_DIR="./"
+DEBIAN_DIR="debian"
 sudo apt-get -y install reprepro
 mkdir -p ${DEBIAN_DIR}/conf
 cp ${HOME}/distributions ${DEBIAN_DIR}/conf/distributions
 reprepro -S utils --basedir ${DEBIAN_DIR} includedeb bullseye ${HOME}/debs/cloud-hypervisor_27.0.0_amd64.deb
 reprepro -S utils --basedir ${DEBIAN_DIR} includedeb bullseye ${HOME}/debs/virtiofsd_1.4.0_amd64.deb
 gpg --output oifv.ai.gpg --armor --export oifv@computelify.ai
-#mv oifv.ai.gpg ${DEBIAN_DIR}
+mv oifv.ai.gpg ${DEBIAN_DIR}
